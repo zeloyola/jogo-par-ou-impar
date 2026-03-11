db8ba9cc241c1c0b25faa559b1d28c456725846a
Hyagoloyola <hyagoloyolaleite@gmail.com>
2026-03-11 15:41:28 -0300

    Criação dos estilos e iniciando o código

---
+++ codigo.js
@@ -0,0 +1,7 @@
+const buttonPar = document.addEventListener('#par')
+const buttonImpar = document.addEventListener('#impar')
+const visor = document.addEventListener('#visor')
+
+function calcularValores(){
+    const val = Math.random * 10
+}
