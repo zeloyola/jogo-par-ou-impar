47d8fb9695008f2814dca4e96eb504c7eb468daa
Hyagoloyola <hyagoloyolaleite@gmail.com>
2026-03-13 16:24:01 -0300

    Finalizando a estilização

--- codigo.js
+++ codigo.js
@@ -1,7 +1,68 @@
-const buttonPar = document.addEventListener('#par')
-const buttonImpar = document.addEventListener('#impar')
-const visor = document.addEventListener('#visor')
+// const buttonPar = document.querySelector('#par')
+// const buttonImpar = document.querySelector('#impar')
+const visor = document.querySelector('#visor')
+const numerosMaos = document.querySelector('#numerosMaos')
+const parOuImpar = document.querySelector('#parOuImpar')
 
-function calcularValores(){
-    const val = Math.random * 10
+let userNumber = undefined
+let escolhaDoJogador = undefined
+
+function escolhendoParOuImpar() {
+    visor.innerHTML = 'Escolha um valor...'
+    numerosMaos.classList.remove('hide')
+    parOuImpar.classList.add('hide')
+
 }
+
+
+
+function calcularValores() {
+    const R = Math.random() * 5
+    const arredondado = Math.round(R)
+    const calcularResto = (arredondado + userNumber) % 2
+    return calcularResto
+}
+// Função para calcular o valor da máquina e calcular o resto para saber se deu par ou ímpar
+
+function asdasd() {
+    const result = calcularValores()
+
+}
+
+function btn_0() {
+    userNumber = 0
+
+}
+
+
+function btn_1() {
+    userNumber = 1
+    asdasd()
+}
+
+function btn_2() {
+    userNumber = 2
+    asdasd()
+}
+
+function btn_3() {
+    userNumber = 3
+    asdasd()
+}
+
+function btn_4() {
+    userNumber = 4
+    asdasd()
+}
+
+function btn_5() {
+    userNumber = 5
+    asdasd()
+}
+
+
+
+// function escolherParOuImpar() {
+//     visor.innerHTML = 'Escolha suas opções... (Par ou Ímpar)'
+//     numerosMaos.classList.remove('hide')
+// }
