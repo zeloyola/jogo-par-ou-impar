803a92fe36ec89ce38cee9e9981a3e675fdf1f91
Hyagoloyola <hyagoloyolaleite@gmail.com>
2026-03-18 15:05:58 -0300

    Finalização do botão "Reiniciar"

--- codigo.js
+++ codigo.js
@@ -1,8 +1,9 @@
 const visor = document.querySelector('#visor')
 const numerosMaos = document.querySelector('#numerosMaos')
 const parOuImpar = document.querySelector('#parOuImpar')
+const buttonReiniciar = document.querySelector('#reset')
 
-let userNumber = undefined
+let userNumber = undefined 
 let escolhaDoJogador = undefined
 
 function escolheuImpar() {
@@ -11,7 +12,9 @@ function escolheuImpar() {
     visor.innerHTML = 'Escolha um valor...'
     numerosMaos.classList.remove('hide')
     parOuImpar.classList.add('hide')
+    buttonReiniciar.classList.add('hide')
 }
+// Botão Ímpar
 
 function escolheuPar() {
     escolhaDoJogador = 0
@@ -19,27 +22,42 @@ function escolheuPar() {
     visor.innerHTML = 'Escolha um valor...'
     numerosMaos.classList.remove('hide')
     parOuImpar.classList.add('hide')
-
+    buttonReiniciar.classList.add('hide')
 }
+// Botão Par
 
 function calcularValores() {
     const R = Math.random() * 5
     const arredondado = Math.round(R)
     const calcularResto = (arredondado + userNumber) % 2
     return calcularResto
+
 }
 // Função para calcular o valor da máquina e calcular o resto para saber se deu par ou ímpar
 
 function oResto() {
     const result = calcularValores()
     const usuarioVenceu = escolhaDoJogador === result
-    if(usuarioVenceu){
-        visor.innerHTML = 'Você venceu!!'
+    if (usuarioVenceu) {
+        visor.innerHTML = `Você venceu!!`
     } else {
         visor.innerHTML = 'Você perdeu!!'
     }
 
+    parOuImpar.classList.add('hide')
+    numerosMaos.classList.add('hide')
+    buttonReiniciar.classList.remove('hide')
+
 }
+// Função para entregar o resultado
+
+function reiniciar() {
+    visor.innerHTML = 'Escolha uma opções... (Par ou Ímpar)'
+    parOuImpar.classList.remove('hide')
+    numerosMaos.classList.add('hide')
+    buttonReiniciar.classList.add('hide')
+}
+// Botão Reiniciar
 
 function btn_0() {
     userNumber = 0
